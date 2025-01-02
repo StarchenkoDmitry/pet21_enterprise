5e3835aaa8fca72a7ea4d7fe23d1137f738f1643
StarchenkDmitry <starchenkodmitrypavlovich@gmail.com>
2025-01-02 19:00:30 +0300

    feat(frontend): add v-for for display names of users

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,42 @@
 <template>
   <h1>Stady vue</h1>
 
+  <p>name: {{ name }}</p>
+
   <input 
     type="text" 
     placeholder="Name" 
-    @input="setName($event.target.value)"
+    v-model="name"
   >
-  <p>name: {{ name }}</p>
-  
+
+  <button
+    @click="addUser()"
+  >Add user</button>
+
+  <!-- <div>{{ users }}</div> -->
+
+  <div v-for="(elm, index) in users" :key="index">
+    <p>{{ elm.name }}</p>
+  </div>
+
 </template>
     
 <script>
 export default {
   data() {
     return {
-      name:"init name"
+      users: [],
+      name:"Dimka"
     }
   },
   methods: {
     setName(newName){
       this.name = newName;
+    },
+    addUser(){
+      this.users.push({
+        name: this.name
+      });
     }
   }
 }
