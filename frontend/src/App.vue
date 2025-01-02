1f997d1aeb0fe7758d510179878796184e2eb4e9
StarchenkDmitry <starchenkodmitrypavlovich@gmail.com>
2025-01-02 18:39:13 +0300

    feat(frontend): #3 of itProger youtube

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,33 @@
-<script>
-</script>
-
 <template>
   <h1>Hi world.</h1>
+  <h2>{{ title }}</h2>
+
+  <button 
+    type="button" 
+    @dblclick="this,title = 'double click'"
+    @click="changeTitle()"
+  >Click on me!</button>
+  <button
+    @click="changeTitle(Math.random())"
+  >
+    Random Title
+  </button>
 </template>
+    
+<script>
+export default {
+  data: () => {
+    return {
+      title: "my first app"
+    };
+  },
+  methods:{
+    changeTitle(newTitle = "My dafulte title."){
+      this.title = newTitle;
+    }
+  }
+}
+</script>
 
 <style scoped>
 </style>
