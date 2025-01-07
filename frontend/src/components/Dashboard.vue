c3d02d81064d0d8e4a8ac5ff12a263942876997a
StarchenkDmitry <starchenkodmitrypavlovich@gmail.com>
2025-01-07 19:25:07 +0300

    stady(frontend): add suspense example 2

---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,12 @@
+<script setup>
+const delay =((Math.random()*3) + 1.5)*1000; 
+const rand = await new Promise((ok)=>setTimeout(
+    ()=>ok(Math.random()),
+    delay,
+));
+console.log("delay:",delay);
+</script>
+
+<template>
+    Suspense rand: {{ rand }}
+</template>
